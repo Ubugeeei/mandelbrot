ba40f1b84e957f0e0719312568737dc1a1711ac0
Ubugeeei <ubuge1122@gamil.com>
2022-08-13 22:05:54 +0900

    impl export as png

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
-use num::Complex;
 use std::fs::File;
+use std::io::Write;
 use std::str::FromStr;
+
+use num::Complex;
 
 extern crate image;
 use image::png::PNGEncoder;
 use image::ColorType;
 
-fn main() {}
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(
+            std::io::stderr(),
+            "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT"
+        )
+        .unwrap();
+        writeln!(
+            std::io::stderr(),
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        )
+        .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+}
 
 fn mandelbrot(c: Complex<f64>, lim: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
@@ -57,7 +84,12 @@ fn write_image(
     let output = File::create(filename)?;
 
     let encoder = PNGEncoder::new(output);
-    let result = encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8));
+    let result = encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    );
     match result {
         Ok(()) => {
             return Ok(());
