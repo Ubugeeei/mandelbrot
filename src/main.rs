d63f2fecea19518eb2defd54ae81133ac07118b5
Ubugeeei <ubuge1122@gamil.com>
2022-08-13 21:17:06 +0900

    impl mandelbrot expr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use num::Complex;
+
+fn main() {}
+
+fn mandelbrot(c: Complex<f64>, lim: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..lim {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
 }
