caceb9ad75b2c412b5507bcacd8b70f20920e9f3
Ubugeeei <ubuge1122@gamil.com>
2022-08-13 21:24:59 +0900

    impl parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {}
 
@@ -11,4 +12,23 @@ fn mandelbrot(c: Complex<f64>, lim: usize) -> Option<usize> {
         z = z * z + c;
     }
     None
+}
+
+/// parse cli args
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+/// Parse a pair of floating-point numbers separated by a comma as a complex number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
+    }
 }
